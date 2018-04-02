2e66566ea2a5c69b204f7b6ae1e9b5eed0149bb2
nanocheeze <cybershrapnel@nanocheeze.com>
2018-04-02 13:34:30 -0700

    Update util.h

--- src/util.h
+++ src/util.h
@@ -8,11 +8,7 @@
 
 #include "uint256.h"
 
-#ifndef WIN32
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#endif
+
 
 #include "tinyformat.h"
 
@@ -208,13 +204,8 @@ boost::filesystem::path GetDefaultDataDir();
 const boost::filesystem::path &GetDataDir(bool fNetSpecific = true);
 boost::filesystem::path GetConfigFile();
 boost::filesystem::path GetPidFile();
-#ifndef WIN32
-void CreatePidFile(const boost::filesystem::path &path, pid_t pid);
-#endif
 void ReadConfigFile(std::map<std::string, std::string>& mapSettingsRet, std::map<std::string, std::vector<std::string> >& mapMultiSettingsRet);
-#ifdef WIN32
 boost::filesystem::path GetSpecialFolderPath(int nFolder, bool fCreate = true);
-#endif
 void ShrinkDebugFile();
 int GetRandInt(int nMax);
 uint64_t GetRand(uint64_t nMax);
